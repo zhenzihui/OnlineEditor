8e14190714f01c5a4e0deac9cfed52fa1a4db40d
zhenzihui <578079901@qq.com>
2017-10-23 16:12:09 +0800

    temporarily backup

--- src/com/zhenz/Controller/ArticleController.java
+++ src/com/zhenz/Controller/ArticleController.java
@@ -1,9 +1,12 @@
 package com.zhenz.Controller;
 
+import com.zhenz.Entity.Article;
 import com.zhenz.Service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Administrator on 2017/10/23.
@@ -13,7 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ArticleController {
     @Autowired
     ArticleService articleService;
-    
+
+    @RequestMapping("/{id}")
+    public ModelAndView get(@RequestParam int id)
+    {
+
+        Article article= articleService.get(id);
+        
+
+    }
+
 
 
 
